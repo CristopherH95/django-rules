cb632d8acc762c487bb1995da1cac744b83b00f9
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2025-09-21 21:53:52 -0700

    Update module sort

--- rules/permissions.py
+++ rules/permissions.py
@@ -1,4 +1,5 @@
 from asgiref.sync import sync_to_async
+
 from .rulesets import RuleSet
 
 permissions = RuleSet()
--- tests/testsuite/test_permissions.py
+++ tests/testsuite/test_permissions.py
@@ -1,5 +1,6 @@
-from asgiref.sync import sync_to_async
 from unittest import TestCase
+
+from asgiref.sync import sync_to_async
 
 from rules.permissions import (
     ObjectPermissionBackend,

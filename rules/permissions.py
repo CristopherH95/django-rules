1144e5489d99424d083a2c75eeabf4f53015582e
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2025-10-17 10:09:06 -0700

    Update formatting

--- rules/permissions.py
+++ rules/permissions.py
@@ -1,11 +1,11 @@
 from .rulesets import RuleSet
 
-
 permissions = RuleSet()
 
 
 async def _run_async(func, *args, **kwargs):
     from asgiref.sync import sync_to_async
+
     return await sync_to_async(func)(*args, **kwargs)
 
 
